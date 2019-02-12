87ffa6a943f7029e307b9a8aa2bde6030736b7ee
Kotensky <stastkach24@gmail.com>
2019-02-13 00:56:37 +0200

    Third commit

--- app/src/main/java/com/kotensky/eggsinsta/FeedAdapter.kt
+++ app/src/main/java/com/kotensky/eggsinsta/FeedAdapter.kt
@@ -3,32 +3,132 @@ package com.kotensky.eggsinsta
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.feed_item.view.*
 
 class FeedAdapter(
-    private val likedIds: ArrayList<Int>,
-    private val listener: OnFeedClickListener
+        private val likedIds: ArrayList<Int>,
+        private val listener: OnFeedClickListener
 ) : RecyclerView.Adapter<FeedAdapter.FeedViewHolder>() {
 
+    private var lastClickTime = 0L
+    private var lastClickPosition = -1
+    var doubleClickItemPosition: Int = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.feed_item, parent, false)
         return FeedViewHolder(view)
     }
 
-    override fun getItemCount() = Int.MAX_VALUE
+    override fun getItemCount() = 100000
 
     override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
         holder.itemView.like_img?.setImageResource(
-            if (likedIds.contains(position))
-                R.drawable.ic_favorite_selected
-            else
-                R.drawable.ic_favorite
+                if (likedIds.contains(position))
+                    R.drawable.ic_favorite_selected
+                else
+                    R.drawable.ic_favorite
         )
+
+        when (position) {
+            0 -> {
+                holder.itemView.item_img?.setImageResource(R.drawable.hello)
+                holder.itemView.title_txt?.setText(R.string.hello)
+            }
+            itemCount - 1 -> {
+                holder.itemView.item_img?.setImageResource(R.drawable.general)
+                holder.itemView.title_txt?.setText(R.string.general)
+            }
+            else -> {
+                holder.itemView.item_img?.setImageResource(R.drawable.egg_0)
+                holder.itemView.title_txt?.setText(R.string.world_record_egg)
+            }
+        }
+
+
+
+        if (doubleClickItemPosition == position) {
+
+            holder.itemView.middle_like_image?.alpha = 0f
+            holder.itemView.middle_like_image?.scaleX = 0.8f
+            holder.itemView.middle_like_image?.scaleY = 0.8f
+
+            holder.itemView.middle_like_image
+                    .animate()
+                    .cancel()
+
+            holder.itemView.middle_like_image
+                    .animate()
+                    .alpha(1f)
+                    .scaleX(1.1f)
+                    .scaleY(1.1f)
+                    .setStartDelay(0)
+                    .setDuration((MIDDLE_LIKE_ANIM_DURATION).toLong())
+                    .withEndAction {
+                        holder.itemView.middle_like_image
+                                .animate()
+                                .scaleX(0.95f)
+                                .scaleY(0.95f)
+                                .setDuration((MIDDLE_LIKE_ANIM_DURATION / 2).toLong())
+                                .withEndAction {
+                                    holder.itemView.middle_like_image
+                                            .animate()
+                                            .scaleX(1f)
+                                            .scaleY(1f)
+                                            .setDuration((MIDDLE_LIKE_ANIM_DURATION / 2).toLong())
+                                            .withEndAction {
+                                                holder.itemView.middle_like_image
+                                                        .animate()
+                                                        .alpha(0f)
+                                                        .scaleX(0.2f)
+                                                        .scaleY(0.2f)
+                                                        .setInterpolator(AccelerateInterpolator())
+                                                        .setDuration((MIDDLE_LIKE_ANIM_DURATION / 2).toLong())
+                                                        .setStartDelay((MIDDLE_LIKE_ANIM_DURATION * 2).toLong())
+                                                        .start()
+                                            }
+                                            .start()
+                                }
+                                .start()
+                    }
+                    .start()
+
+            doubleClickItemPosition = -1
+
+        } else if (holder.itemView.middle_like_image?.alpha ?: 0f > 0f) {
+            holder.itemView.middle_like_image
+                    .animate()
+                    .cancel()
+
+            holder.itemView.middle_like_image
+                    .animate()
+                    .alpha(0f)
+                    .setDuration((MIDDLE_LIKE_ANIM_DURATION * 2).toLong())
+                    .start()
+        }
+
+        holder.itemView.more_img?.setOnClickListener {
+            listener.onMoreClick(position)
+        }
 
         holder.itemView.like_img?.setOnClickListener {
             listener.onLikeClick(position)
+        }
+
+
+        holder.itemView.item_img?.setOnClickListener {
+            if (lastClickPosition == position &&
+                    System.currentTimeMillis() - lastClickTime <= DOUBLE_CLICK_INTERVAL_MILLIS) {
+
+                listener.onImageDoubleClick(position)
+
+                lastClickPosition = -1
+                lastClickTime = 0L
+            } else {
+                lastClickPosition = position
+                lastClickTime = System.currentTimeMillis()
+            }
         }
 
         holder.itemView.item_like_count_txt.text = likedIds.size.toString()
@@ -39,5 +139,11 @@ class FeedAdapter(
     interface OnFeedClickListener {
         fun onImageDoubleClick(position: Int)
         fun onLikeClick(position: Int)
+        fun onMoreClick(position: Int)
+    }
+
+    companion object {
+        const val DOUBLE_CLICK_INTERVAL_MILLIS = 400
+        const val MIDDLE_LIKE_ANIM_DURATION = 400
     }
 }
--- app/src/main/java/com/kotensky/eggsinsta/MainActivity.kt
+++ app/src/main/java/com/kotensky/eggsinsta/MainActivity.kt
@@ -3,9 +3,13 @@ package com.kotensky.eggsinsta
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.more_dialog.*
+import android.content.Intent
+import android.net.Uri
 
 
 class MainActivity : AppCompatActivity(), FeedAdapter.OnFeedClickListener {
@@ -16,6 +20,8 @@ class MainActivity : AppCompatActivity(), FeedAdapter.OnFeedClickListener {
     private lateinit var prefs: SharedPreferences
 
     private val likedIds = ArrayList<Int>()
+    private var moreDialog: AlertDialog? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,24 +32,52 @@ class MainActivity : AppCompatActivity(), FeedAdapter.OnFeedClickListener {
         adapter = FeedAdapter(likedIds, this)
         layoutManager = LinearLayoutManager(this)
 
-
         likedIds.addAll(stringToIds(prefs.getString(LIKED_IDS_KEY, "")))
 
         main_rv.adapter = adapter
         main_rv.layoutManager = layoutManager
-        main_rv.scrollToPosition(prefs.getInt(CURRENT_POSITION_KEY, Int.MAX_VALUE / 2))
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val savedLikedIds = stringToIds(prefs.getString(LIKED_IDS_KEY, ""))
-
-        if (likedIds.size != savedLikedIds.size || !likedIds.containsAll(savedLikedIds)) {
-            likedIds.clear()
-            likedIds.addAll(savedLikedIds)
-            adapter.notifyDataSetChanged()
+        main_rv.post {
+            main_rv?.scrollToPosition(prefs.getInt(CURRENT_POSITION_KEY, adapter.itemCount / 2))
         }
+
+        setupMoreDialog()
     }
+
+    private fun setupMoreDialog(){
+        val appPackageName = packageName
+
+        val dialogView = layoutInflater.inflate(R.layout.more_dialog, null)
+        dialogView?.rate_app_txt?.setOnClickListener {
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")))
+            } catch (anfe: android.content.ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")))
+            }
+            moreDialog?.dismiss()
+        }
+
+        dialogView?.share_txt?.setOnClickListener {
+            try {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.type = "text/plain"
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name))
+                var shareMessage = "\nLet me recommend you this application\n\n"
+                shareMessage += "https://play.google.com/store/apps/details?id=$appPackageName\n\n"
+                shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+                startActivity(Intent.createChooser(shareIntent, "Share app via..."))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            moreDialog?.dismiss()
+        }
+
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialogView)
+        moreDialog = builder.create()
+    }
+
+
 
     private fun stringToIds(string: String?): ArrayList<Int> {
         val newList = ArrayList<Int>()
@@ -54,15 +88,6 @@ class MainActivity : AppCompatActivity(), FeedAdapter.OnFeedClickListener {
             newList.add(id)
         }
         return newList
-    }
-
-    override fun onPause() {
-        val editor = prefs.edit()
-        editor.putString(LIKED_IDS_KEY, likesToString())
-        editor.putInt(CURRENT_POSITION_KEY, layoutManager.findFirstVisibleItemPosition())
-        editor.apply()
-
-        super.onPause()
     }
 
     private fun likesToString(): String {
@@ -76,8 +101,38 @@ class MainActivity : AppCompatActivity(), FeedAdapter.OnFeedClickListener {
         return sb.toString()
     }
 
+    override fun onResume() {
+        super.onResume()
+        val savedLikedIds = stringToIds(prefs.getString(LIKED_IDS_KEY, ""))
+
+        if (likedIds.size != savedLikedIds.size || !likedIds.containsAll(savedLikedIds)) {
+            likedIds.clear()
+            likedIds.addAll(savedLikedIds)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    override fun onPause() {
+        val editor = prefs.edit()
+        editor.putString(LIKED_IDS_KEY, likesToString())
+        editor.putInt(CURRENT_POSITION_KEY, layoutManager.findFirstVisibleItemPosition())
+
+        editor.apply()
+
+        super.onPause()
+    }
+
     override fun onImageDoubleClick(position: Int) {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (!likedIds.contains(position)) {
+            likedIds.add(position)
+        }
+        adapter.doubleClickItemPosition = position
+        adapter.notifyDataSetChanged()
+
+    }
+
+    override fun onMoreClick(position: Int) {
+        moreDialog?.show()
     }
 
     override fun onLikeClick(position: Int) {
